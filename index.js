b943d52f75eea4daff5fd9143b13cb3ece9a63b0
SilverSokolova <xtechresearchcenter2@gmail.com>
2024-05-28 04:43:36 -0500

    optimize images

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
 const itemData = {
-  "": "",
-  "dried_mushroom": "A less-than-tasty dried mushroom.<br>Replenishes 1 HP.",
-  "mushroom": "A feel-good mushroom.<br>Replenishes 5 HP.",
-  "honey_syrup": "A sweet snack. Restores 5 FP.",
-  "tasty_tonic": "A tasty medicine. Cures poison and<br>other ailments.",
-  "fire_flower": "Attacks all enemies with fireballs that<br>do 3 damage.",
-  "sleepy_sheep": "Temporarily puts all enemies to sleep,<br>immobilizing them.",
-  "fright_mask": "Summons a scary spirit that may<br>chase enemies away.",
-  "horsetail": "A plant found in Petal Meadows.<br>Replenishes 3 HP.",
+  "": "(No item)",
+  "dried_mushroom": "<strong>Dried Mushroom</strong><br>A less-than-tasty dried mushroom.<br>Replenishes 1 HP.",
+  "mushroom": "<strong>Mushroom</strong><br>A feel-good mushroom.<br>Replenishes 5 HP.",
+  "super_mushroom": "<strong>Super Mushroom</strong><br>A feel-super mushroom.<br>Replenishes 10 HP.",
+  "life_mushroom": "<strong>Life Mushroom</strong><br>Restores 10 HP when Mario or his partner falls.",
+  "honey_syrup": "<strong>Honey Syrup</strong><br>A sweet snack. Restores 5 FP.",
+  "tasty_tonic": "<strong>Tasty Tonic</strong><br>A tasty medicine. Cures poison and<br>other ailments.",
+  "fire_flower": "<strong>Fire Flower</strong><br>Attacks all enemies with fireballs that<br>do 3 damage. Does not actually burn enemies.",
+  "ice_storm": "<strong>Ice Storm</strong><br>Blows a cold wind at all enemies that<br>does 3 damage and freezes them.",
+  "sleepy_sheep": "<strong>Sleepy Sheep</strong><br>Temporarily puts all enemies to sleep,<br>immobilizing them.",
+  "fright_mask": "<strong>Fright Mask</strong><br>Summons a scary spirit that may<br>chase enemies away.",
+  "horsetail": "<strong>Horsetail</strong><br>A plant found in Petal Meadows.<br>Replenishes 3 HP.",
+  "courage_shell": "<strong>Courage Shell</strong><br>Temporarily gives a party member<br>courage to boost their Defense.",
+  "thunder_bolt": "<strong>Thunder Bolt</strong><br>Drops lightning on an enemy and<br>does 5 damage.",
+  "inn_coupon": "<strong>Inn Coupon</strong><br>A free pass! Lets you stay one free<br>night at an inn.",
+  "dizzy_dial": "<strong>Dizzy Dial</strong><br>Temporarily makes all enemies dizzy,<br>decreasing their accuracy.",
 
-  "mushroom_fry": "A tasty dish made by Zess T.<br>Replenishes 6 HP and 2 FP.",
-  "mushroom_steak": "A tasty dish made by Zess T.<br>Replenishes 30 HP and 10 FP.",
-  "fresh_juice": "A Zess T. juice. Cures conditions like<br>poisoning and replenishes 5 FP.",
-  "mistake": "Zess T.'s failed attempt.<br>Replenishes 1 HP and 1 FP."
+  "mushroom_fry": "<strong>Mushroom Fry</strong><br>A tasty dish made by Zess T.<br>Replenishes 6 HP and 2 FP.<br><em>Made by cooking a Mushroom or a<br>Super Mushroom.</em>",
+  "mushroom_roast": "<strong>Mushroom Roast</strong><br>A tasty dish made by Zess T.<br>Replenishes 15 HP and 5 FP.",
+  "mushroom_steak": "<strong>Mushroom Steak</strong><br>A tasty dish made by Zess T.<br>Replenishes 30 HP and 10 FP.",
+  "fresh_juice": "<strong>Fresh Juice</strong><br>A Zess T. juice. Cures conditions like<br>poisoning and replenishes 5 FP.<br><em>Made by steeping Honey Syrup.</em>",
+  "spicy_soup": "<strong>Spicy Soup</strong><br>Yummy soup made by Zess T.<br>Replenishes 4 HP and 4 FP.<br><em>Made by cooking a Fire Flower.</em>",
+
+  "honey_mushroom": "<strong>Honey Mushroom</strong><br>A dessert made by Zess T.<br>Replenishes 5 HP and 5 FP.",
+
+  "mystery_output": "<strong>ALMOST ALWAYS GIVES A MISTAKE. But can rarely give:</strong><br> Honey Mushroom, Life Mushroom",
+  "mystery": "<strong>Mystery</strong><br>Who knows what this does? Take a<br>chance and find out!",
+  "mistake": "<strong>Mistake</strong><br>Zess T.'s failed attempt.<br>Replenishes 1 HP and 1 FP.<br><em>Made by mixing incompatible<br>ingredients.</em>"
 };
 
 /*
 So for translations, we'll do like [itemNames("fr", "mushroom")] or something? Does my US copy even allow me to change language?
 */
 const options = [
+  ["Dried Mushroom", "dried_mushroom"],
   ["Mushroom", "mushroom"],
+  ["Super Mushroom", "super_mushroom"],
+  ["Life Mushroom", "life_mushroom"],
   ["Honey Syrup", "honey_syrup"],
   ["Tasty Tonic", "tasty_tonic"],
   ["Fire Flower", "fire_flower"],
+  ["Ice Storm", "ice_storm"],
   ["Sleepy Sheep", "sleepy_sheep"],
   ["Fright Mask", "fright_mask"],
   ["Horsetail", "horsetail"],
+  ["Courage Shell", "courage_shell"],
+  ["Thunder Bolt", "thunder_bolt"],
+  ["Inn Coupon", "inn_coupon"],
 
   ["Mushroom Fry", "mushroom_fry"],
+  ["Mushroom Roast", "mushroom_roast"],
   ["Mushroom Steak", "mushroom_steak"],
+
+  ["Fresh Juice", "fresh_juice"],
+  ["Spicy Soup", "spicy_soup"],
+
+  ["Honey Mushroom", "honey_mushroom"],
+  ["Mystery", "mystery"],
   ["Mistake", "mistake"]
 ];
 
@@ -37,9 +65,9 @@ const recipes = {
   "-super_mushroom": "mushroom_fry",
   "-ultra_mushroom": "mushroom_steak",
   "-honey_syrup": "fresh_juice",
-  "-mushroom": "mushroom_fry",
-  "-mushroom": "mushroom_fry",
-  "-mushroom": "mushroom_fry",
+  "-fire_flower": "spicy_soup",
+  "-mystery": "mystery_output",
+  "-life_mushroom": "mushroom_roast",
   "-mushroom": "mushroom_fry",
 }
 
